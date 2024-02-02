772061995246428cd17b827035786dce0f89eefb
devnizamuddin <dev.nizamuddin@gmail.com>
2024-02-02 23:55:07 +0600

    single row item componet added

---
+++ lib/app/components/single_row_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SingleRowItem extends StatelessWidget {
+  const SingleRowItem({
+    super.key,
+    required this.title,
+    this.value,
+    this.titleTextStyle,
+    this.valueTextStyle,
+  });
+  final String title;
+  final String? value;
+  final TextStyle? titleTextStyle;
+  final TextStyle? valueTextStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(child: Text(title, style: titleTextStyle)),
+        const Text(': '),
+        Expanded(
+            child: Text(
+          value ?? '',
+          style: valueTextStyle,
+          textAlign: TextAlign.end,
+        )),
+      ],
+    );
+  }
+}
